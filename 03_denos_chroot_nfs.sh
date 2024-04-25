827193f2ca41c21eba37bb729d134552d2536cfd
Jeff Loughridge <jeffl@alumni.duke.edu>
2024-04-25 10:51:12 -0400

    Update 03_denos_chroot_nfs.sh
    Add packages, fix misc issues

--- 03_denos_chroot_nfs.sh
+++ 03_denos_chroot_nfs.sh
@@ -2,12 +2,9 @@
 # Bernardino Lopez [ bernardino.lopez@gmail.com ]
 # 11.27.18
 
-# Modified by Jeff Loughridge
-
-# 03_denos_chroot_nfs.sh - Customize your Distro
-# Preps system for NFS booting including NFS /home
+# 03_denos_chroot.sh - Customize your Distro
 # As root in chroot. Execute the script in a Terminal
-# ./03_denos_chroot_nfs.sh
+# ./03_denos_chroot.sh
 
 # export LIVE_BOOT=LIVE_BOOT64
 source ./denos_config.txt
@@ -17,29 +14,34 @@ echo $DISTRO_HOSTNAME > /etc/hostname
 
 apt-cache search linux-image
 
-apt-get update && \
+apt-get update
+
 apt-get install -y --no-install-recommends \
     linux-image-amd64 \
     live-boot \
     systemd-sysv \
-    nfs-common
+    nfs-common \
+    nfs-kernel-server \
+    iproute2
 
 apt-get install -y --no-install-recommends \
     curl openssh-server openssh-client \
     iputils-ping \
     htop nmap wget git ca-certificates \
-    nano fdisk && \
+    nano fdisk firmware-linux-free
+
 apt-get clean
 
 echo -e "127.0.0.1\tlocalhost" > /etc/hosts
 echo -e "127.0.0.1\t$DISTRO_HOSTNAME" >> /etc/hosts
 
-# Create live user
-#useradd -m live -s /bin/bash
-# Change user live password to : newpassword
-#echo 'jeffl:sprint26' | chpasswd
 
+echo "Set root password"
 passwd root
+
+echo "Set $NONROOTUSER password"
+passwd $NONROOTUSER
+useradd $NONROOTUSER -s /bin/bash -G sudo
 
 cat << EOF > /etc/fstab
 proc    /proc   proc    defaults        0 0
@@ -54,8 +56,6 @@ locale-gen LANG=en_US.UTF-8
 locale-gen --purge "en_US.UTF-8"
 dpkg-reconfigure --frontend noninteractive locales
 
-apt install -y firmware-linux-free
-
 cat << EOF > /etc/initramfs-tools/initramfs.conf
 MODULES=netboot
 BUSYBOX=y
@@ -69,5 +69,3 @@ EOF
 update-initramfs -u
 echo ASYNMOUNTNFS=no >> /etc/default/rcS
 echo RAMTMP=yes >> /etc/default/tmpfs
-
-exit
